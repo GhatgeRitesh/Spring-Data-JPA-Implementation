9d33a6305f8cdf7d4907896e6cddef07ff61dd89
GhatgeRitesh <riteshghatge12345@gmail.com>
2024-05-10 19:12:13 +0530

    simple curd successfull form remaining

--- src/test/java/com/example/demo/ApplicationTests.java
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.demo;
 
+import com.example.demo.Model.UserDAO;
+import com.example.demo.Model.UserEntity;
+import lombok.extern.java.Log;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
+@Log
 class ApplicationTests {
 
+	@Autowired
+	private UserDAO userDAO;
 	@Test
-	void contextLoads() {
+	void addUser() {
+		log.info("Test started");
+		UserEntity user=new UserEntity();
+		user.setName("Test3");
+		user.setDept("IT");
+		user.setEmail("test3@gmail.com");
+		log.info("Test Finished");
+		userDAO.save(user);
+	}
+
+	@Test
+	void getAllUserAndDelete()
+	{
+		log.info("test 2 started");
+		List<UserEntity> user= userDAO.getAllUserEntities();
+		log.info("the Users are :"+user.toString());
+		for( UserEntity users:user)
+		{
+			userDAO.delete(users);
+		}
+		log.info("test 2 finished");
 	}
 
 }
